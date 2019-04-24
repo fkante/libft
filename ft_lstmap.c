2a5f8d2c5b6f306f8ce68d120ba61a0eefd29c73
Francis KANTE <fkante@e3r4p3.42.fr>
2019-04-24 17:23:50 +0200

    1st lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,30 +6,25 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 14:21:53 by fkante            #+#    #+#             */
-/*   Updated: 2019/04/24 15:19:56 by fkante           ###   ########.fr       */
+/*   Updated: 2019/04/24 17:23:10 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_lst_len(t_list *lst)
-{
-	size_t i;
-	
-	i = 0;
-	while (lst->next != NULL)
-	{
-		i++;
-		lst = lst->next;
-	}
-	return (i);
-}
-
-
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	size_t lst_len;
-
-	lst_len = ft_lst_len(lst);
-	return (0);
+	t_list *ls_new;
+	
+	if (lst != NULL && f != NULL)
+	{
+		if (!(f(lst)))
+			return (NULL);
+		lst = lst->next;
+		while (lst->next != NULL)
+		{
+			
+		}
+	}
+	return (NULL);
 }
